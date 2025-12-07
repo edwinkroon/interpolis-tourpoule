88d17a936f2036707150bf76de8e36c245e6ef18
Edwin Kroon <edwin.kroon@outlook.com>
2025-12-07 12:03:20 +0100

    Add welcome pages, authentication setup, and Auth0 integration

---
+++ auth-config.js
@@ -0,0 +1,6 @@
+window.AUTH0_CONFIG = {
+    domain: 'dev-g1uy3ps8fzt6ic37.us.auth0.com',
+    clientId: '4WLxdHDBodGyZB7Tbi3WRqECFqlbYeTO',
+    redirectUri: 'https://interpolistourpoule.netlify.app/auth-callback.html'
+  };
+  
